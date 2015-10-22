cbf6da0554b7c348f5c7c9a2612665297fb6edc6
EWB User <ewb@unassigned-hostname>
2015-10-22 17:03:31 -0400

    Added Directional movement with character

--- project.js
+++ project.js
@@ -2,6 +2,8 @@ title('My Project');
 
 // put variables here, just like kahn academy
 var exampleVar = 100;
+var charX = 100;
+var charY = 460;
 
 // put functions here, just like kahn academy
 var exampleFunction = function() {
@@ -18,7 +20,14 @@ var exampleFunction = function() {
 
 // this code is executed once when the program is started
 var setup = function() {
-
+	/*
+	music = new Audio("blank.wav");
+	music.controls = true;
+	music.loop = true;                      FOR MUSIC USE
+	music.autoplay = true;
+	document.body.appendChild(music);
+	*/
+	char = loadImage("swiggityswag.png");
     // set up the size of the canvas (you probably don't want to change this!)
     size(800, 600);
 
@@ -27,20 +36,30 @@ var setup = function() {
 // override draw function, by default it will be called 60 times per second
 var draw = function() {
 
-    // clear the screen
-    background(255, 255, 255);
+        // clear the screen
+        background(255, 255, 0);
 
-    // some drawing commands; feel free to change these
-    fill(255, 0, 0);
+	image(char, charX, charY);
+	    if(charY > 600) {
+		charY = charY - 600;
+	    }
 
-    // draw some text
-    textSize(50);
-    text("Hello, World!", exampleVar, exampleVar);
+	    if(charX > 0 && keyIsPressed && keyCode == LEFT ) {
+		charX = charX - 25;
+	    }
 
-    textSize(20);
-    text("Press the space bar...", 500, 500);
+	    if(charX < 700 && keyIsPressed && keyCode == RIGHT ) {
+		charX = charX + 25;
+	    }
+	    if(charX > 0 && keyIsPressed && keyCode == UP ) {
+		charY = charY - 25;
+	    }
 
-    // call a function (defined up above)
-    exampleFunction();
+	    if(charY < 700 && keyIsPressed && keyCode == DOWN ) {
+		charY = charY + 25;
+	    }
+
+	
+	
 
 };
