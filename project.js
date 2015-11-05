89ba7d0a0bdc94866c600b8ee40c33af2e95102a
EWB User <ewb@unassigned-hostname>
2015-11-05 17:50:53 -0500

    Reached peak of code; now creating level design & left and right wall scene5&4 parameters :D

--- project.js
+++ project.js
@@ -4,21 +4,75 @@ title('My Project');
 var exampleVar = 100;
 var charX = 100;
 var charY = 460;
+var currentScene = 5;
 
-// put functions here, just like kahn academy
-var exampleFunction = function() {
-    
-    // change color depending on key pressed
-    if (keyIsPressed && keyCode === SPACE) {
-        fill(255, 0, 0);
-    } else {
-        fill(0, 0, 255);
-    }
 
-    rect(300, 200, 55, 55);
+var drawscene1 = function() {
+    currentScene = 1;
+    background(255, 255, 255);
+    text("1", 178, 20);
+   
 };
 
-// this code is executed once when the program is started
+var drawscene2 = function() {
+    currentScene = 2;
+    background(255, 255, 255);
+    text("2", 178, 20);
+   
+};
+
+var drawscene3 = function() {
+    currentScene = 3;
+    background(255, 255, 255);
+    text("3", 178, 20);
+   
+};
+
+var drawscene4 = function() {
+    currentScene = 4;
+    background(255, 255, 255);
+    text("4", 178, 20);
+   
+};
+
+var drawscene5 = function() {
+    currentScene = 5;
+     background(0, 200, 200);
+    text("5", 178, 20);
+    
+   
+};
+
+var drawscene6 = function() {
+    currentScene = 6;
+     background(0, 200, 200);
+    text("6", 178, 20);
+   
+};
+
+var drawscene7 = function() {
+    currentScene = 7;
+    background(255, 255, 255);
+    text("7", 178, 20);
+   
+};
+
+var drawscene8 = function() {
+    currentScene = 8;
+    background(255, 255, 255);
+    text("8", 178, 20);
+   
+};
+
+var drawscene9 = function() {
+    currentScene = 9;
+    background(255, 255, 255);
+    text("9", 178, 20);
+   
+};
+
+
+    
 var setup = function() {
 	/*
 	music = new Audio("blank.wav");
@@ -27,39 +81,50 @@ var setup = function() {
 	music.autoplay = true;
 	document.body.appendChild(music);
 	*/
-	char = loadImage("swiggityswag.png");
+	char = loadImage("rsz_swiggityswag.png");
     // set up the size of the canvas (you probably don't want to change this!)
     size(800, 600);
-
+        // clear the screen
 };
 
-// override draw function, by default it will be called 60 times per second
+
 var draw = function() {
 
-        // clear the screen
+    drawscene5();
 
-    background(124,0,0);
     
-    (char, charX, charY);
+    
+   
+
+	image(char, charX, charY);
 	    if(charY > 600) {
 		charY = charY - 600;
 	    }
 
-	    if(charX > 0 && keyIsPressed && keyCode == LEFT ) {
+	    if(keyIsPressed && keyCode == LEFT ) {
 		charX = charX - 25;
-	    }
+            
+		
+	    };
 
-	    if(charX < 700 && keyIsPressed && keyCode == RIGHT ) {
+            if(//add if hits left side and establish parameters (currentscene to move a scene tadadadadadda :D
+    
+	    if(keyIsPressed && keyCode == RIGHT ) {
 		charX = charX + 25;
 	    }
-	    if(charX > 50 && keyIsPressed && keyCode == UP ) {
+            
+
+
+
+
+      /*      if(charX > 0 && keyIsPressed && keyCode == UP ) {
 		charY = charY - 25;
 	    }
 
-	    if(charY < 600 && keyIsPressed && keyCode == DOWN ) {
+	    if(charY < 700 && keyIsPressed && keyCode == DOWN ) {
 		charY = charY + 25;
 	    }
-
+	    */
 	
 	
 
